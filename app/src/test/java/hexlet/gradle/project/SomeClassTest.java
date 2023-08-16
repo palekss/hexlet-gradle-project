63676acee42b61f1837a92bf5cd9ac4133beec78
pas <perepelkin.aleks@gmail.com>
2023-08-16 20:38:14 +0300

    simple Gradle project with JUnit5

---
+++ app/src/test/java/hexlet/gradle/project/SomeClassTest.java
@@ -0,0 +1,12 @@
+package hexlet.gradle.project;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class SomeClassTest {
+    @Test
+    public void testSum() {
+        var expected = 5;
+        var actual = SomeClass.sum(3, 2);
+        assertEquals(expected, actual);
+    }
+}
